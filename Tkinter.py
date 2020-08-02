199fe11a99bbdcec68235967037f28b5a1456360
“Tu <robertocorondocoto@gmail.com>
2020-08-01 22:10:55 -0500

    Se agrego un boton

--- Tkinter.py
+++ Tkinter.py
@@ -2,7 +2,12 @@ import tkinter
 
 ventana = tkinter.Tk()
 ventana.geometry("600x600")
-etiqueta =tkinter.Label(ventana, text ="Guadalajara te espera", bg="blue")
-etiqueta.pack(fill =tkinter.Y,expand=True)
+
+etiqueta =tkinter.Label(ventana, text ="Guadalajara te espera", bg="gray")
+etiqueta.pack(fill =tkinter.X,)
+
+
+boton1=tkinter.Button(ventana,text="Click aqui")
+boton1.pack()
 
 ventana.mainloop()
