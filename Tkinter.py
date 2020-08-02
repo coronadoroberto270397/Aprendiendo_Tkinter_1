9c6c8dc4a6c833f9f46f8174202589234a0ccbf5
“Tu <robertocorondocoto@gmail.com>
2020-08-01 22:15:54 -0500

    Se agrego una funcion a Tkinter

--- Tkinter.py
+++ Tkinter.py
@@ -6,8 +6,9 @@ ventana.geometry("600x600")
 etiqueta =tkinter.Label(ventana, text ="Guadalajara te espera", bg="gray")
 etiqueta.pack(fill =tkinter.X,)
 
-
-boton1=tkinter.Button(ventana,text="Click aqui")
+def saludo():
+    print ("Hola amigo")
+boton1=tkinter.Button(ventana,text="Click aqui", command = saludo)
 boton1.pack()
 
 ventana.mainloop()
